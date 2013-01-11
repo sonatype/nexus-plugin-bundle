079f5c02161968848ab6a80f0ba77b5615abc26b
Jason Dillon <jason@planet57.com>
2013-01-11 15:29:59 -0800

    Tidy

--- maven-plugin/src/main/java/org/sonatype/nexus/pluginbundle/maven/GenerateMetadataMojo.java
+++ maven-plugin/src/main/java/org/sonatype/nexus/pluginbundle/maven/GenerateMetadataMojo.java
@@ -36,7 +36,6 @@ import org.sonatype.nexus.pluginbundle.maven.scm.HgDebugIdCommand;
 import org.sonatype.nexus.pluginbundle.maven.scm.HgDebugIdScmResult;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
