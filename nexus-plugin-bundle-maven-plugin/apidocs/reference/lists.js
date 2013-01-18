3f082a13b1969e23a5b13366e11105fa4d89072d
Jason Dillon <jason@planet57.com>
2013-01-18 10:06:16 -0800

    Wagon: Deploying site to repository

---
+++ nexus-plugin-bundle-maven-plugin/apidocs/reference/lists.js
@@ -0,0 +1,5 @@
+var DATA = [
+      { id:0, label:"org.sonatype.nexus.pluginbundle.maven", link:"org/sonatype/nexus/pluginbundle/maven/package-summary.html", type:"package" },
+      { id:1, label:"org.sonatype.nexus.pluginbundle.maven.DummyTest", link:"org/sonatype/nexus/pluginbundle/maven/DummyTest.html", type:"class" }
+
+    ];
