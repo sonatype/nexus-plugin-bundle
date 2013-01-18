11c018556cbacaa1da76bbf6ee84da0166666777
Jason Dillon <jason@planet57.com>
2013-01-18 11:18:43 -0800

    Wagon: Deploying site to repository

--- nexus-plugin-bundle-maven-plugin/apidocs/assets/navtree_data.js
+++ nexus-plugin-bundle-maven-plugin/apidocs/assets/navtree_data.js
@@ -1,5 +1,9 @@
 var NAVTREE_DATA =
-[ [ "org.sonatype.nexus.pluginbundle.maven", "org/sonatype/nexus/pluginbundle/maven/package-summary.html", [ [ "Classes", null, [ [ "DummyTest", "org/sonatype/nexus/pluginbundle/maven/DummyTest.html", null, "" ] ]
+[ [ "default package", "default package/package-summary.html", [ [ "Classes", null, [ [ "HelpMojo", "default package/HelpMojo.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "org.sonatype.nexus.pluginbundle.maven", "org/sonatype/nexus/pluginbundle/maven/package-summary.html", [ [ "Classes", null, [ [ "BundleConfiguration", "org/sonatype/nexus/pluginbundle/maven/BundleConfiguration.html", null, "" ], [ "CheckDependenciesMojo", "org/sonatype/nexus/pluginbundle/maven/CheckDependenciesMojo.html", null, "" ], [ "ClasspathUtils", "org/sonatype/nexus/pluginbundle/maven/ClasspathUtils.html", null, "" ], [ "CreateBundleMojo", "org/sonatype/nexus/pluginbundle/maven/CreateBundleMojo.html", null, "" ], [ "GAVCoordinate", "org/sonatype/nexus/pluginbundle/maven/GAVCoordinate.html", null, "" ], [ "GenerateMetadataMojo", "org/sonatype/nexus/pluginbundle/maven/GenerateMetadataMojo.html", null, "" ], [ "MojoSupport", "org/sonatype/nexus/pluginbundle/maven/MojoSupport.html", null, "" ], [ "PluginDescriptorGenerationRequest", "org/sonatype/nexus/pluginbundle/maven/PluginDescriptorGenerationRequest.html", null, "" ], [ "PluginDescriptorGenerator", "org/sonatype/nexus/pluginbundle/maven/PluginDescriptorGenerator.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "org.sonatype.nexus.pluginbundle.maven.scm", "org/sonatype/nexus/pluginbundle/maven/scm/package-summary.html", [ [ "Classes", null, [ [ "GitRevParseCommand", "org/sonatype/nexus/pluginbundle/maven/scm/GitRevParseCommand.html", null, "" ], [ "GitRevParseScmResult", "org/sonatype/nexus/pluginbundle/maven/scm/GitRevParseScmResult.html", null, "" ], [ "HgDebugIdCommand", "org/sonatype/nexus/pluginbundle/maven/scm/HgDebugIdCommand.html", null, "" ], [ "HgDebugIdScmResult", "org/sonatype/nexus/pluginbundle/maven/scm/HgDebugIdScmResult.html", null, "" ] ]
 , "" ] ]
 , "" ] ]
 
--- nexus-plugin-bundle-maven-plugin/apidocs/reference/lists.js
+++ nexus-plugin-bundle-maven-plugin/apidocs/reference/lists.js
@@ -1,5 +1,20 @@
 var DATA = [
-      { id:0, label:"org.sonatype.nexus.pluginbundle.maven", link:"org/sonatype/nexus/pluginbundle/maven/package-summary.html", type:"package" },
-      { id:1, label:"org.sonatype.nexus.pluginbundle.maven.DummyTest", link:"org/sonatype/nexus/pluginbundle/maven/DummyTest.html", type:"class" }
+      { id:0, label:"HelpMojo", link:"default package/HelpMojo.html", type:"class" },
+      { id:1, label:"default package", link:"default package/package-summary.html", type:"package" },
+      { id:2, label:"org.sonatype.nexus.pluginbundle.maven", link:"org/sonatype/nexus/pluginbundle/maven/package-summary.html", type:"package" },
+      { id:3, label:"org.sonatype.nexus.pluginbundle.maven.BundleConfiguration", link:"org/sonatype/nexus/pluginbundle/maven/BundleConfiguration.html", type:"class" },
+      { id:4, label:"org.sonatype.nexus.pluginbundle.maven.CheckDependenciesMojo", link:"org/sonatype/nexus/pluginbundle/maven/CheckDependenciesMojo.html", type:"class" },
+      { id:5, label:"org.sonatype.nexus.pluginbundle.maven.ClasspathUtils", link:"org/sonatype/nexus/pluginbundle/maven/ClasspathUtils.html", type:"class" },
+      { id:6, label:"org.sonatype.nexus.pluginbundle.maven.CreateBundleMojo", link:"org/sonatype/nexus/pluginbundle/maven/CreateBundleMojo.html", type:"class" },
+      { id:7, label:"org.sonatype.nexus.pluginbundle.maven.GAVCoordinate", link:"org/sonatype/nexus/pluginbundle/maven/GAVCoordinate.html", type:"class" },
+      { id:8, label:"org.sonatype.nexus.pluginbundle.maven.GenerateMetadataMojo", link:"org/sonatype/nexus/pluginbundle/maven/GenerateMetadataMojo.html", type:"class" },
+      { id:9, label:"org.sonatype.nexus.pluginbundle.maven.MojoSupport", link:"org/sonatype/nexus/pluginbundle/maven/MojoSupport.html", type:"class" },
+      { id:10, label:"org.sonatype.nexus.pluginbundle.maven.PluginDescriptorGenerationRequest", link:"org/sonatype/nexus/pluginbundle/maven/PluginDescriptorGenerationRequest.html", type:"class" },
+      { id:11, label:"org.sonatype.nexus.pluginbundle.maven.PluginDescriptorGenerator", link:"org/sonatype/nexus/pluginbundle/maven/PluginDescriptorGenerator.html", type:"class" },
+      { id:12, label:"org.sonatype.nexus.pluginbundle.maven.scm", link:"org/sonatype/nexus/pluginbundle/maven/scm/package-summary.html", type:"package" },
+      { id:13, label:"org.sonatype.nexus.pluginbundle.maven.scm.GitRevParseCommand", link:"org/sonatype/nexus/pluginbundle/maven/scm/GitRevParseCommand.html", type:"class" },
+      { id:14, label:"org.sonatype.nexus.pluginbundle.maven.scm.GitRevParseScmResult", link:"org/sonatype/nexus/pluginbundle/maven/scm/GitRevParseScmResult.html", type:"class" },
+      { id:15, label:"org.sonatype.nexus.pluginbundle.maven.scm.HgDebugIdCommand", link:"org/sonatype/nexus/pluginbundle/maven/scm/HgDebugIdCommand.html", type:"class" },
+      { id:16, label:"org.sonatype.nexus.pluginbundle.maven.scm.HgDebugIdScmResult", link:"org/sonatype/nexus/pluginbundle/maven/scm/HgDebugIdScmResult.html", type:"class" }
 
     ];
