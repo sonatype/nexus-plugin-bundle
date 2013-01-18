5422077e1ab63e57a1b38c6887b6f69b43a9ce91
Jason Dillon <jason@planet57.com>
2013-01-18 10:04:09 -0800

    Wagon: Deploying site to repository

---
+++ apidocs/assets/navtree_data.js
@@ -0,0 +1,7 @@
+var NAVTREE_DATA =
+[ [ "org.sonatype.nexus.pluginbundle.maven", "org/sonatype/nexus/pluginbundle/maven/package-summary.html", [ [ "Classes", null, [ [ "DummyTest", "org/sonatype/nexus/pluginbundle/maven/DummyTest.html", null, "" ] ]
+, "" ] ]
+, "" ] ]
+
+;
+
