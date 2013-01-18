314ee0d882f51d1e6c8c76e498e1a2d86cae9d37
Jason Dillon <jason@planet57.com>
2013-01-18 10:05:03 -0800

    Wagon: Deploying site to repository

---
+++ nexus-plugin-bundle-model/apidocs/assets/navtree_data.js
@@ -0,0 +1,13 @@
+var NAVTREE_DATA =
+[ [ "org.sonatype.plugins.model", "org/sonatype/plugins/model/package-summary.html", [ [ "Classes", null, [ [ "ClasspathDependency", "org/sonatype/plugins/model/ClasspathDependency.html", null, "" ], [ "PluginDependency", "org/sonatype/plugins/model/PluginDependency.html", null, "" ], [ "PluginLicense", "org/sonatype/plugins/model/PluginLicense.html", null, "" ], [ "PluginMetadata", "org/sonatype/plugins/model/PluginMetadata.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "org.sonatype.plugins.model.io.xpp3", "org/sonatype/plugins/model/io/xpp3/package-summary.html", [ [ "Classes", null, [ [ "PluginModelXpp3Reader", "org/sonatype/plugins/model/io/xpp3/PluginModelXpp3Reader.html", null, "" ], [ "PluginModelXpp3Writer", "org/sonatype/plugins/model/io/xpp3/PluginModelXpp3Writer.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "org.sonatype.plugins.model.v1_0_0", "org/sonatype/plugins/model/v1_0_0/package-summary.html", [ [ "Classes", null, [ [ "ClasspathDependency", "org/sonatype/plugins/model/v1_0_0/ClasspathDependency.html", null, "" ], [ "PluginDependency", "org/sonatype/plugins/model/v1_0_0/PluginDependency.html", null, "" ], [ "PluginLicense", "org/sonatype/plugins/model/v1_0_0/PluginLicense.html", null, "" ], [ "PluginMetadata", "org/sonatype/plugins/model/v1_0_0/PluginMetadata.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "org.sonatype.plugins.model.v1_0_0.io.xpp3", "org/sonatype/plugins/model/v1_0_0/io/xpp3/package-summary.html", [ [ "Classes", null, [ [ "PluginModelXpp3Reader", "org/sonatype/plugins/model/v1_0_0/io/xpp3/PluginModelXpp3Reader.html", null, "" ], [ "PluginModelXpp3Writer", "org/sonatype/plugins/model/v1_0_0/io/xpp3/PluginModelXpp3Writer.html", null, "" ] ]
+, "" ] ]
+, "" ] ]
+
+;
+
---
+++ nexus-plugin-bundle-model/apidocs/reference/lists.js
@@ -0,0 +1,19 @@
+var DATA = [
+      { id:0, label:"org.sonatype.plugins.model", link:"org/sonatype/plugins/model/package-summary.html", type:"package" },
+      { id:1, label:"org.sonatype.plugins.model.ClasspathDependency", link:"org/sonatype/plugins/model/ClasspathDependency.html", type:"class" },
+      { id:2, label:"org.sonatype.plugins.model.PluginDependency", link:"org/sonatype/plugins/model/PluginDependency.html", type:"class" },
+      { id:3, label:"org.sonatype.plugins.model.PluginLicense", link:"org/sonatype/plugins/model/PluginLicense.html", type:"class" },
+      { id:4, label:"org.sonatype.plugins.model.PluginMetadata", link:"org/sonatype/plugins/model/PluginMetadata.html", type:"class" },
+      { id:5, label:"org.sonatype.plugins.model.io.xpp3", link:"org/sonatype/plugins/model/io/xpp3/package-summary.html", type:"package" },
+      { id:6, label:"org.sonatype.plugins.model.io.xpp3.PluginModelXpp3Reader", link:"org/sonatype/plugins/model/io/xpp3/PluginModelXpp3Reader.html", type:"class" },
+      { id:7, label:"org.sonatype.plugins.model.io.xpp3.PluginModelXpp3Writer", link:"org/sonatype/plugins/model/io/xpp3/PluginModelXpp3Writer.html", type:"class" },
+      { id:8, label:"org.sonatype.plugins.model.v1_0_0", link:"org/sonatype/plugins/model/v1_0_0/package-summary.html", type:"package" },
+      { id:9, label:"org.sonatype.plugins.model.v1_0_0.ClasspathDependency", link:"org/sonatype/plugins/model/v1_0_0/ClasspathDependency.html", type:"class" },
+      { id:10, label:"org.sonatype.plugins.model.v1_0_0.PluginDependency", link:"org/sonatype/plugins/model/v1_0_0/PluginDependency.html", type:"class" },
+      { id:11, label:"org.sonatype.plugins.model.v1_0_0.PluginLicense", link:"org/sonatype/plugins/model/v1_0_0/PluginLicense.html", type:"class" },
+      { id:12, label:"org.sonatype.plugins.model.v1_0_0.PluginMetadata", link:"org/sonatype/plugins/model/v1_0_0/PluginMetadata.html", type:"class" },
+      { id:13, label:"org.sonatype.plugins.model.v1_0_0.io.xpp3", link:"org/sonatype/plugins/model/v1_0_0/io/xpp3/package-summary.html", type:"package" },
+      { id:14, label:"org.sonatype.plugins.model.v1_0_0.io.xpp3.PluginModelXpp3Reader", link:"org/sonatype/plugins/model/v1_0_0/io/xpp3/PluginModelXpp3Reader.html", type:"class" },
+      { id:15, label:"org.sonatype.plugins.model.v1_0_0.io.xpp3.PluginModelXpp3Writer", link:"org/sonatype/plugins/model/v1_0_0/io/xpp3/PluginModelXpp3Writer.html", type:"class" }
+
+    ];
